07332b5757f49675786b4e666130e5f187a08935
Rik Hemsley <rik@rikkus.info>
2015-12-21 15:18:54 +0000

    Update 10.cs

--- 10.cs
+++ 10.cs
@@ -1,1 +1,7 @@
-Enumerable.Range(1, 50).Aggregate("1113122113", (acc, i) => Regex.Replace(acc, @"(.)\1*", m => (m.Value.Length + m.Groups[1].Value))).Length
+Enumerable.Range(1, 50)
+  .Aggregate
+  (
+    "1113122113",
+    (acc, i) =>
+    Regex.Replace(acc, @"(.)\1*", m => (m.Value.Length + m.Groups[1].Value))
+  ).Length
