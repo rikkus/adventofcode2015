dd2ba9e655ec4d2a8c2880083f4295e3b0a9cc87
Rik Hemsley <rik@rikkus.info>
2015-12-21 15:18:12 +0000

    Update 10.cs

--- 10.cs
+++ 10.cs
@@ -1,1 +1,1 @@
-Regex.Replace(input, @"(.)\1*", m => (m.Value.Length + m.Groups[1].Value))
+Enumerable.Range(1, 50).Aggregate("1113122113", (acc, i) => Regex.Replace(acc, @"(.)\1*", m => (m.Value.Length + m.Groups[1].Value))).Length
