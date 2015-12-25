257f839b985f49f562c37bb7e8132f82f37a494c
Rik Hemsley <rik@rikkus.info>
2015-12-25 22:00:20 +0000

    Create 16-1.cs

---
+++ 16-1.cs
@@ -0,0 +1,47 @@
+	var sues = input.Select(i =>
+
+		{
+			var tokens = i.Split(new[] { ':' }, 2);
+
+			var dict = Regex.Matches
+			(
+				tokens[1],
+				@"(?<name>\w+):\s(?<n>\d+)",
+				RegexOptions.ExplicitCapture
+				| RegexOptions.IgnorePatternWhitespace
+				| RegexOptions.CultureInvariant
+				| RegexOptions.Compiled
+			)
+			.Cast<Match>()
+			.Select
+			(
+				m =>
+				new
+				{
+					Name = m.Groups["name"].Value,
+					Value = int.Parse(m.Groups["n"].Value)
+				}
+			)
+			.ToDictionary(o => o.Name, o => o.Value);
+
+			dict["i"] = int.Parse(tokens[0].Split(new[] { ' ' })[1]);
+
+			return dict;
+		}
+	);
+
+	var known = new Dictionary<string, int>()
+	{
+		{ "children", 3 },
+		{ "cats", 7 },
+		{ "samoyeds", 2 },
+		{ "pomeranians", 3 },
+		{ "akitas", 0 },
+		{ "vizslas", 0 },
+		{ "goldfish", 5 },
+		{ "trees", 3 },
+		{ "cars", 2 },
+		{ "perfumes", 1 }
+	};
+
+	sues.Where(sue => known.Keys.All(key => !sue.ContainsKey(key) || sue[key] == known[key])).Dump();
